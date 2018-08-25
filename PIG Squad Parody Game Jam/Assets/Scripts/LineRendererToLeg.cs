7ead7208950721bfdf90e54d482a0ab7199749ca
BOGOSLAF\Henry <henry.schaefer@outlook.com>
2018-08-24 21:27:35 -0700

    leggy boi is leggin

---
+++ PIG Squad Parody Game Jam/Assets/Scripts/LineRendererToLeg.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRendererToLeg : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+    public List<Transform> legJoints = new List<Transform>();
+
+    private List<Vector3> legJointPositions = new List<Vector3>();
+
+    void Start()
+    {
+        lineRenderer.positionCount = legJoints.Count;
+        UpdateLineRendererPoints();
+    }
+
+    private void Update()
+    {
+        UpdateLineRendererPoints();
+    }
+
+    private void UpdateLineRendererPoints()
+    {
+        for (int i = 0; i < legJoints.Count; i++)
+        {
+            lineRenderer.SetPosition(i, legJoints[i].position);
+        }
+    }
+
+
+
+    public void EditorSetLineRenderer()
+    {
+        Start();
+    }
+}
---
+++ PIG Squad Parody Game Jam/Assets/Scripts/LineRendererToLegEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LineRendererToLeg))]
+public class LineRendererToLegEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        LineRendererToLeg lineRendererToLeg = (LineRendererToLeg)target;
+
+        if (GUILayout.Button("Set Line Renderer"))
+        {
+            lineRendererToLeg.EditorSetLineRenderer();
+        }
+    }
+    public void OnSceneGUI()
+    {
+        
+    }
+
+}
