f00814949ecb23e5129791b1a14ad84533585a5d
BOGOSLAF\Henry <henry.schaefer@outlook.com>
2018-08-26 15:31:25 -0700

    hands be workin

--- PIG Squad Parody Game Jam/Assets/Scripts/SetRotationFromDirection.cs
+++ PIG Squad Parody Game Jam/Assets/Scripts/SetRotationFromDirection.cs
@@ -10,7 +10,7 @@ public class SetRotationFromDirection : MonoBehaviour
     void Update()
     {
         Vector3 direction = transform.position - joint.transform.position;
-        transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = Quaternion.LookRotation(direction, Vector3.forward) * Quaternion.LookRotation(Vector3.up);
         //transform.rotation = Quaternion.LookRotation(direction) * Quaternion.LookRotation(Vector3.right);
     }
 }
