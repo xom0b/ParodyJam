97db7b550d5086e76659770a9e6ea0d38a5baeff
BOGOSLAF\Henry <henry.schaefer@outlook.com>
2018-08-26 15:26:10 -0700

    added background shit

---
+++ PIG Squad Parody Game Jam/Assets/Scripts/SetRotationFromDirection.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetRotationFromDirection : MonoBehaviour
+{
+    public GameObject joint;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = transform.position - joint.transform.position;
+        transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
+        //transform.rotation = Quaternion.LookRotation(direction) * Quaternion.LookRotation(Vector3.right);
+    }
+}
