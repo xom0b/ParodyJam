891f640941e0a666e00be2d56c0d02abf7970497
BOGOSLAF\Henry <henry.schaefer@outlook.com>
2018-08-26 22:03:30 -0700

    tweaked spawn variables

--- PIG Squad Parody Game Jam/Assets/Scripts/LineRendererToLegEditor.cs
+++ PIG Squad Parody Game Jam/Assets/Scripts/LineRendererToLegEditor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
 using UnityEditor;
 
 [CustomEditor(typeof(LineRendererToLeg))]
@@ -20,5 +21,5 @@ public class LineRendererToLegEditor : Editor
     {
         
     }
-
 }
+#endif
